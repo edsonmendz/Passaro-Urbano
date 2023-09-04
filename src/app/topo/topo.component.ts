723590b35f8b7138662f7f8a432597884c70aed4
edsonmendz <japaact@gmail.com>
2023-09-03 22:35:19 -0300

    Promises

---
+++ src/app/topo/topo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'urbano-topo',
+  templateUrl: './topo.component.html',
+  styleUrls: ['./topo.component.css']
+})
+export class TopoComponent {
+
+}
