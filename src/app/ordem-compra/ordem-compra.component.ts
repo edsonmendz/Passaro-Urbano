78fe5fe366b15662b50879173bbb93b482701427
edsonmendz <japaact@gmail.com>
2023-09-18 23:22:55 -0300

    two-way-binding em formulário

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class OrdemCompraComponent {
 
+  public endereco: string = ''
+  public numero: string = ''
+  public complemento: string = ''
+  public formaPagamento: string = ''
+
+  constructor() {
+
+  }
+
+  public atualizaEndereco(endereco:string):void {
+    this.endereco = endereco
+    console.log(this.endereco)
+  }
+
+  public atualizaNumero(numero:string):void {
+    this.numero = numero
+    console.log(this.numero)
+  }
+
+  public atualizaComplemento(complemento:string):void {
+    this.complemento = complemento
+    console.log(this.complemento)
+  }
+
+  public atualizaFormaPagamento(formaPagamento:string):void {
+    this.formaPagamento = formaPagamento
+    console.log(this.formaPagamento)
+  }
+
 }
